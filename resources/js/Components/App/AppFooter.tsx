802b968753a16fc7bdfa5b9efc27a44f923df573
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-08-30 07:32:24 +0200

    Extract app footer

---
+++ resources/js/Components/App/AppFooter.tsx
@@ -0,0 +1,90 @@
+import { Globe } from "lucide-react";
+import { GlobalProps } from "@/Types";
+import { Link, usePage } from "@inertiajs/react";
+import { useTranslationsStore } from "@narsil-localization/Stores/translationStore";
+import * as React from "react";
+import AppCopyright from "@narsil-ui/Components/App/AppCopyright";
+import AppLanguage from "@narsil-localization/Components/App/AppLanguage";
+import AppVersion from "@narsil-ui/Components/App/AppVersion";
+import Button from "@narsil-ui/Components/Button/Button";
+import Container from "@narsil-ui/Components/Container/Container";
+import NavigationMenu from "@narsil-ui/Components/NavigationMenu/NavigationMenu";
+import NavigationMenuItem from "@narsil-ui/Components/NavigationMenu/NavigationMenuItem";
+import NavigationMenuList from "@narsil-ui/Components/NavigationMenu/NavigationMenuList";
+
+interface AppFooterProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const AppFooter = React.forwardRef<HTMLDivElement, AppFooterProps>(({ ...props }, ref) => {
+	const { trans } = useTranslationsStore();
+
+	const shared = usePage<GlobalProps>().props.shared;
+
+	return (
+		<footer
+			ref={ref}
+			{...props}
+		>
+			<div className='bg-primary-highlight text-primary-highlight-foreground'>
+				<Container className='grid grid-cols-1 lg:grid-cols-2'>
+					<AppLanguage
+						className='place-self-start self-center bg-transparent hover:bg-transparent'
+						languages={shared.localization.languages}
+						format='long'
+						chevron={true}
+						size='default'
+						variant='link'
+					>
+						<Globe />
+					</AppLanguage>
+					<NavigationMenu className='place-self-start lg:place-self-end'>
+						<NavigationMenuList className='flex flex-wrap gap-4'>
+							<NavigationMenuItem>
+								<Button
+									asChild={true}
+									size='default'
+									variant='link'
+								>
+									<Link
+										href={route("imprint")}
+										method='get'
+									>
+										{trans("Imprint")}
+									</Link>
+								</Button>
+							</NavigationMenuItem>
+							<NavigationMenuItem>
+								<Button
+									asChild={true}
+									size='default'
+									variant='link'
+								>
+									<Link
+										href={route("privacy-notice")}
+										method='get'
+									>
+										{trans("Privacy notice")}
+									</Link>
+								</Button>
+							</NavigationMenuItem>
+						</NavigationMenuList>
+					</NavigationMenu>
+				</Container>
+			</div>
+
+			<div className='bg-primary text-primary-foreground'>
+				<Container className='grid grid-cols-2 py-4'>
+					<AppCopyright
+						className='place-self-start self-center'
+						name={shared.app.name ?? "Narsil"}
+					/>
+					<AppVersion
+						className='place-self-end self-center'
+						version={shared.app.version ?? "1.0.0"}
+					/>
+				</Container>
+			</div>
+		</footer>
+	);
+});
+
+export default AppFooter;
--- resources/js/Components/Layout/WebLayout.tsx
+++ resources/js/Components/Layout/WebLayout.tsx
@@ -6,28 +6,25 @@ import { navigationMenuTriggerStyle } from "@narsil-ui/Components/NavigationMenu
 import { useTranslationsStore } from "@narsil-localization/Stores/translationStore";
 import { useWindowScroll } from "react-use";
 import * as React from "react";
-import AppCopyright from "@narsil-ui/Components/App/AppCopyright";
+import AppFooter from "@/Components/App/AppFooter";
 import AppLanguage from "@narsil-localization/Components/App/AppLanguage";
-import AppVersion from "@narsil-ui/Components/App/AppVersion";
 import Button from "@narsil-ui/Components/Button/Button";
 import Container from "@narsil-ui/Components/Container/Container";
 import DropdownMenu from "@narsil-ui/Components/DropdownMenu/DropdownMenu";
 import DropdownMenuItem from "@narsil-ui/Components/DropdownMenu/DropdownMenuItem";
 import Layout from "@narsil-ui/Components/Layout/Layout";
-import NavigationMenu from "@narsil-ui/Components/NavigationMenu/NavigationMenu";
 import NavigationMenuItem from "@narsil-ui/Components/NavigationMenu/NavigationMenuItem";
-import NavigationMenuList from "@narsil-ui/Components/NavigationMenu/NavigationMenuList";
 import Sheet from "@narsil-ui/Components/Sheet/Sheet";
 import SheetClose from "@narsil-ui/Components/Sheet/SheetClose";
+import SheetDescription from "@narsil-ui/Components/Sheet/SheetDescription";
+import SheetHeader from "@narsil-ui/Components/Sheet/SheetHeader";
 import SheetPortal from "@narsil-ui/Components/Sheet/SheetPortal";
+import SheetTitle from "@narsil-ui/Components/Sheet/SheetTitle";
 import ThemeController from "@narsil-ui/Components/Themes/ThemeController";
 import UserMenuDropdownContent from "@narsil-auth/Components/UserMenu/UserMenuDropdownContent";
 import UserMenuSheetContent from "@narsil-auth/Components/UserMenu/UserMenuSheetContent";
 import UserMenuTrigger from "@narsil-auth/Components/UserMenu/UserMenuTrigger";
 import useScreenStore from "@narsil-ui/Stores/screenStore";
-import SheetHeader from "@narsil-ui/Components/Sheet/SheetHeader";
-import SheetTitle from "@narsil-ui/Components/Sheet/SheetTitle";
-import SheetDescription from "@narsil-ui/Components/Sheet/SheetDescription";
 
 interface Props {
 	children?: React.ReactNode;
@@ -177,67 +174,7 @@ const WebLayout = ({ children }: Props) => {
 					{children}
 				</Container>
 
-				<footer className='absolute bottom-0 w-full translate-y-full'>
-					<div className='bg-primary-highlight text-primary-highlight-foreground'>
-						<Container className='grid grid-cols-1 lg:grid-cols-2'>
-							<AppLanguage
-								className='place-self-start self-center bg-transparent hover:bg-transparent'
-								languages={shared.localization.languages}
-								format='long'
-								chevron={true}
-								size='default'
-								variant='link'
-							>
-								<Globe />
-							</AppLanguage>
-							<NavigationMenu className='place-self-start lg:place-self-end'>
-								<NavigationMenuList className='flex flex-wrap gap-4'>
-									<NavigationMenuItem>
-										<Button
-											asChild={true}
-											size='default'
-											variant='link'
-										>
-											<Link
-												href={route("imprint")}
-												method='get'
-											>
-												{trans("Imprint")}
-											</Link>
-										</Button>
-									</NavigationMenuItem>
-									<NavigationMenuItem>
-										<Button
-											asChild={true}
-											size='default'
-											variant='link'
-										>
-											<Link
-												href={route("privacy-notice")}
-												method='get'
-											>
-												{trans("Privacy notice")}
-											</Link>
-										</Button>
-									</NavigationMenuItem>
-								</NavigationMenuList>
-							</NavigationMenu>
-						</Container>
-					</div>
-
-					<div className='bg-primary text-primary-foreground'>
-						<Container className='grid grid-cols-2 py-4'>
-							<AppCopyright
-								className='place-self-start self-center'
-								name={shared.app.name ?? "Narsil"}
-							/>
-							<AppVersion
-								className='place-self-end self-center'
-								version={shared.app.version ?? "1.0.0"}
-							/>
-						</Container>
-					</div>
-				</footer>
+				<AppFooter className='absolute bottom-0 w-full translate-y-full' />
 			</Sheet>
 		</Layout>
 	);
