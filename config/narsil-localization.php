94a99283708c16d97ee79837ce7ab01d74b4acb7
Narsilien <rigaux.jonathan@gmail.com>
2024-09-08 12:51:11 +0200

    Fix localizations

--- config/narsil-localization.php
+++ config/narsil-localization.php
@@ -30,6 +30,7 @@ return [
         'auth::validation',
         'contacts::validation',
         'forms::validation',
+        'localization::locales',
         'menus::validation',
         'tables::messages',
     ],
