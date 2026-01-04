e78dde1525be85e0d28900b8d3ff3c08446edb20
Narsilien <rigaux.jonathan@gmail.com>
2026-01-04 19:01:06 +0100

    Refactor CMS blocks

--- resources/js/pages/backend/index.tsx
+++ resources/js/pages/backend/index.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "@narsil-cms/blocks";
+import { Heading } from "@narsil-cms/blocks/heading";
 import { SectionContent, SectionHeader, SectionRoot } from "@narsil-cms/components/section";
 
 function Dashboard() {
