aada290c64bab86021de6ec710fa5d1e93cb9b7c
Narsilien <rigaux.jonathan@gmail.com>
2026-01-06 19:29:47 +0100

    Clean up types

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -1,14 +1,3 @@
-import { Fieldset, FormTab, Input } from "@narsil-cms/types";
-
-export type FormType = {
-  action: string;
-  description: string;
-  id: string;
-  submitLabel: string;
-  tabs: (Fieldset | FormTab | Input)[];
-  title: string;
-};
-
 export type GlobalProps = {
   footer: App.Http.Data.FooterData;
   header: App.Http.Data.HeaderData;
