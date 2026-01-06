32a8ec524a081a742ecba50de4172816f7d01fe7
Narsilien <rigaux.jonathan@gmail.com>
2026-01-06 14:54:01 +0100

    Rename policy columns

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -36,9 +36,9 @@ final class UserSeeder extends Seeder
     private function createSuperAdminRole(): Role
     {
         $role = Role::firstOrCreate([
-            Role::HANDLE => 'super_admin',
+            Role::SLUG => 'super_admin',
         ], [
-            Role::NAME => 'Super Admin',
+            Role::LABEL => 'Super Admin',
         ]);
 
         return $role;
