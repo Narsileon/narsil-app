8d4c32010d2e7a5af2a358ef7540ba21a7ae43d2
Narsilien <rigaux.jonathan@gmail.com>
2026-01-30 20:14:00 +0100

    Clean up labels

--- resources/js/components/label/index.ts
+++
@@ -1,6 +0,0 @@
-// https://www.radix-ui.com/primitives/docs/components/label
-
-import LabelRequired from "./label-required";
-import LabelRoot from "./label-root";
-
-export { LabelRequired, LabelRoot };
