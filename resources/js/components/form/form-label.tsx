e7b4edab1720677ece52cf5a3411530599c6256f
Narsilien <rigaux.jonathan@gmail.com>
2026-02-01 13:03:34 +0100

    Move label translation to top level

--- resources/js/components/form/form-label.tsx
+++ resources/js/components/form/form-label.tsx
@@ -1,17 +1,20 @@
-import { Label } from "@narsil-cms/blocks/label";
+import { Label } from "@narsil-cms/components/label";
+import { useLocalization } from "@narsil-cms/components/localization";
 import { cn } from "@narsil-cms/lib/utils";
 import { type ComponentProps } from "react";
 import useFormField from "./form-field-context";
 
 function FormLabel({ children, className, ...props }: ComponentProps<typeof Label>) {
   const { error, handle } = useFormField();
+  const { trans } = useLocalization();
 
   return (
     <Label
-      data-slot="form-label"
       data-error={!!error}
-      className={cn("data-[error=true]:text-destructive min-h-7 items-center", className)}
+      data-slot="form-label"
+      className={cn("min-h-7 items-center data-[error=true]:text-destructive", className)}
       htmlFor={handle}
+      requiredLabel={trans("ui.required")}
       {...props}
     >
       <span className="first-letter:uppercase">{children}</span>
