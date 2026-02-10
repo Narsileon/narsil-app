8be9a3f83c15bc5def7348c43dff7a869991524f
Narsilien <rigaux.jonathan@gmail.com>
2026-02-10 18:18:27 +0100

    Update packages

--- app/Http/Data/FormData.php
+++ app/Http/Data/FormData.php
@@ -5,8 +5,8 @@ namespace App\Http\Data;
 #region USE
 
 use Illuminate\Support\Str;
+use Narsil\Base\Support\TranslationsBag;
 use Narsil\Cms\Form\Models\Form;
-use Narsil\Ui\Support\TranslationsBag;
 use Spatie\LaravelData\Attributes\Computed;
 use Spatie\LaravelData\Data;
 use Spatie\TypeScriptTransformer\Attributes\TypeScript;
--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig(({ mode }) => {
       alias: {
         "@": path.join(__dirname, "/resources/js"),
         "@narsil-cms": path.join(__dirname, "/vendor/narsil/cms/resources/js"),
-        "@narsil-ui": path.join(__dirname, "/vendor/narsil/ui/resources/js"),
+        "@narsil-ui": path.join(__dirname, "/vendor/narsil/base/resources/js"),
       },
       preserveSymlinks: true,
     },
