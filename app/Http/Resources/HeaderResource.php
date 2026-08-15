40c6cf07887d64f1fbd1fbab168a5857920bfe44
Narsilien <rigaux.jonathan@gmail.com>
2026-08-15 16:39:23 +0200

    Add missing regions

--- app/Http/Resources/HeaderResource.php
+++ app/Http/Resources/HeaderResource.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
+#region USE
+
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+#endregion
+
 final class HeaderResource extends JsonResource
 {
+    #region PUBLIC METHODS
+
     /**
      * @param Request $request
      *
@@ -18,4 +24,6 @@ final class HeaderResource extends JsonResource
     {
         return [];
     }
+
+    #endregion
 }
