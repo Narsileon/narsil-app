e87e9394b0b962799755b61e884c3caff16059e1
Narsilien <rigaux.jonathan@gmail.com>
2026-09-05 13:05:43 +0200

    WIP

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <header
 	class="bg-layout text-layout-foreground sticky left-0 right-0 top-0 z-10 flex w-full items-center justify-between px-4 py-2 md:px-4 md:py-4 lg:px-14 xl:px-20"
-	x-data="{ open: false }"
+	x-data="{ navigationOpen: false }"
 >
 	<a
 		class="text-lg font-bold"
@@ -9,7 +9,7 @@
 		NARSIL
 	</a>
 	<button
-		@click="open = !open"
+		@click="navigationOpen = !navigationOpen"
 		aria-label="Toggle navigation"
 		class="md:hidden"
 		type="button"
@@ -20,7 +20,7 @@
 	</button>
 	<x-narsil::ui.navigation-menu.navigation-menu-root
 		class="bg-layout absolute left-0 right-0 top-full p-4 md:static md:block md:bg-transparent md:p-0"
-		x-bind:class="open ? 'block' : 'hidden md:block'"
+		x-bind:class="navigationOpen ? 'block' : 'hidden md:block'"
 	>
 		<x-narsil::ui.navigation-menu.navigation-menu-list
 			class="flex-col items-stretch gap-4 font-bold md:flex-row md:items-center lg:gap-8"
