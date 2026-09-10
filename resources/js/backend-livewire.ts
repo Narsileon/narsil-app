415727bb1438fe8d9a782af788deb00081f52a06
Narsilien <rigaux.jonathan@gmail.com>
2026-09-10 06:26:02 +0200

    WIP

--- resources/js/backend-livewire.ts
+++ resources/js/backend-livewire.ts
@@ -1,11 +1,9 @@
-import anchor from "@alpinejs/anchor";
-import collapse from "@alpinejs/collapse";
 import sort from "@alpinejs/sort";
 import registerAlpineComponents from "@narsil-ui/alpine/components";
 import registerAlpineStores from "@narsil-ui/alpine/stores";
 import { Alpine, Livewire } from "../../vendor/livewire/livewire/dist/livewire.esm.js";
 
-Alpine.plugin([anchor, collapse, sort]);
+Alpine.plugin(sort);
 
 registerAlpineStores(Alpine);
 registerAlpineComponents(Alpine);
